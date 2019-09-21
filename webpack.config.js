10c0e2009635b5160da0b16180ae1fb8a4f40269
takara1356 <goblin7283@gmail.com>
2019-09-21 16:27:21 +0900

    babel

--- webpack.config.js
+++ webpack.config.js
@@ -5,15 +5,41 @@ module.exports = {
     devServer: {
         open: true,
         openPage: "index.html",
-        contentBase: path.join(__dirname, "public"),
+        contentBase: path.join(__dirname, 'public'),
         watchContentBase: true,
         port: 8080,
     },
     entry: {app: './src/index.js'},
     output: {
+        path: path.join(__dirname, "dist"),
         publicPath: "/js/",
         filename: '[name].js',
         library: ["com", "example"],
-        libraryTarget: 'umd'
-    }
+        libraryTarget: 'umd',
+        
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /(node_modules|bower_components)/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: [
+                            [
+                                "@babel/preset-env",
+                                {
+                                    "useBuiltIns": "usage",
+                                    "targets": "> 0.25%, not dead"
+                                }
+                            ]
+                        ]
+                    }
+                }
+            }
+        ]
+    },
+    devtool: 'inline-source-map'
+
 };
